a4dc194cd248fea389e044e680e87606367fbfda
Mhicolo <140319704+Mhicolo@users.noreply.github.com>
2024-11-10 22:01:07 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,25 +4,25 @@
 //Instagram: techgod143
 //Telegram: t.me/techgod143
 //GitHub: @techgod143
-//WhatsApp: +917466008456
+//WhatsApp: +2348035936169
 //want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@techgod143
 const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "910000000000"
-global.ownername = "🐛Tech God"
-global.ytname = "YT: Tech-God"
-global.socialm = "GitHub: techgod143"
-global.location = "India, Up, Rampur"
+global.ownernomer = "2348035936169"
+global.ownername = "🐛Mhicolo"
+global.ytname = "YT: mhicolo"
+global.socialm = "GitHub: mhicolo"
+global.location = "Africa,lagos"
 
-global.ownernumber = '917466008456'  //creator number
-global.ownername = '🐛Tech god' //owner name
-global.botname = 'Tech God ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
+global.ownernumber = '2348035936169'  //creator number
+global.ownername = '🐛mhocolo' //owner name
+global.botname = 'mhicolo bot ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'Tech-God\n\nContact: +917466008456'
+global.author = 'Tech-God\n\nContact: +2348035936169'
 
 //console view/theme
 global.themeemoji = '🪀'
